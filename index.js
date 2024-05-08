760c906ec1938c257978c56d5bef43f0cc21fb1b
minhhoanggggg1 <giuppy2007pun0suy@hotmail.com>
2024-05-08 23:13:24 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-function maxArea(height) {
-  let maxArea = 0;
-  let left = 0;
-  let right = height.length - 1;
-  while (left < right) {
-    maxArea = Math.max(
-      maxArea,
-      Math.min(height[left], height[right]) * (right - left),
-    );
-    if (height[left] < height[right]) left++;
-    else right--;
+function maxSubArray(nums) {
+  let maxSum = nums[0];
+  let currentSum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(maxSum, currentSum);
   }
-  return maxArea;
+  return maxSum;
 }
