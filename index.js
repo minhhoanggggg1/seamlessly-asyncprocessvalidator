b94bc1b29573f22baea8c4efc904948f5302663d
minhhoanggggg1 <giuppy2007pun0suy@hotmail.com>
2024-05-09 15:15:47 +0700

    feat project

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function maxSubArray(nums) {
-  let maxSum = nums[0];
-  let currentSum = nums[0];
-  for (let i = 1; i < nums.length; i++) {
-    currentSum = Math.max(nums[i], currentSum + nums[i]);
-    maxSum = Math.max(maxSum, currentSum);
+function calculate(s) {
+  const stack = [];
+  let num = 0;
+  let sign = "+";
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (!isNaN(parseInt(char)) && char !== " ") {
+      num = num * 10 + parseInt(char);
+    }
+    if (isNaN(parseInt(char)) || i === s.length - 1) {
+      if (sign === "+") stack.push(num);
+      else if (sign === "-") stack.push(-num);
+      else if (sign === "*") stack.push(stack.pop() * num);
+      else if (sign === "/") stack.push(parseInt(stack.pop() / num));
+      num = 0;
+      sign = char;
+    }
   }
-  return maxSum;
+  return stack.reduce((acc, val) => acc + val, 0);
 }
