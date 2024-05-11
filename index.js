1bf32680b035fa408d6b374e141eb3c473e95129
minhhoanggggg1 <giuppy2007pun0suy@hotmail.com>
2024-05-11 22:19:05 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const zip = (arr1, arr2) => arr1.map((el, i) => [el, arr2[i]]);
+function reverseBetween(head, m, n) {
+  if (!head || m === n) return head;
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let prev = dummy;
+  for (let i = 0; i < m - 1; i++) prev = prev.next;
+  let start = prev.next;
+  let then = start.next;
+  for (let i = 0; i < n - m; i++) {
+    start.next = then.next;
+    then.next = prev.next;
+    prev.next = then;
+    then = start.next;
+  }
+  return dummy.next;
+}
